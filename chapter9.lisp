25fd9b29db3d2c93aede437212d2399898775246
DRChrist <dennis_christensen87@hotmail.com>
2019-06-08 21:47:51 +0200

    finished 1., 2. and 3. in chapter9

--- chapter9.lisp
+++ chapter9.lisp
@@ -1,5 +1,5 @@
 (defpackage "CHAPTER9"
-  (:use "COMMON-LISP" "RAYTRACING" "SPHERES" "MATHUTIL")
+  (:use "COMMON-LISP" "RAYTRACING" "MATHUTIL")
   (:export "RAY-TEST"))
 
 (in-package chapter9)
@@ -15,3 +15,37 @@
         ((> z 7))
       (defsphere (* x 200) 300 (* z -400) 40 .75)))
   (tracer (make-pathname :name "spheres.pgm") res))
+
+
+;;;1.
+(defun non-decreasingp (lst)
+  (if (null (cdr lst))
+      t
+      (if (<= (car lst) (car (cdr lst)))
+          (non-decreasingp (cdr lst))
+          nil)))
+
+;;;2.
+(defun make-change (int)
+  (multiple-value-bind (c25 rest25)
+      (floor int 25)
+    (multiple-value-bind (c10 rest10)
+        (floor rest25 10)
+      (multiple-value-bind (c5 c1)
+          (floor rest10 5)
+        (values c25 c10 c5 c1)))))
+
+
+;;;3.
+(defun song-contest ()
+  (do ((count 0 (incf count))
+       (res (list (cons 'wigglie 0) (cons 'wobblie 0)))
+       (winner (random 2) (random 2)))
+      ((= count 10) res)
+    (if (= winner 1)
+        (incf (cdr (assoc 'wigglie res)))
+        (incf (cdr (assoc 'wobblie res))))))
+
+;;;4.
+(defun find-intersection (qx qy wx wy tx ty fx fy)
+  )
