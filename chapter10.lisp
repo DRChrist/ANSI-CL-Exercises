d25d6a7c627b31f5dba8860159346e783c068907
DRChrist <dennis_christensen87@hotmail.com>
2019-07-07 20:02:35 +0200

    Finish 2. and 3. and work on 4. in chapter10

--- chapter10.lisp
+++ chapter10.lisp
@@ -2,11 +2,61 @@
 
 ;;;1.
 ;;(a)
-`(,z ,x z)
+;`(,z ,x z)
 ;;(b)
-`(x ,y ,@z)
+;`(x ,y ,@z)
 ;;(c)
-`((,@z ,x) z)
+;`((,@z ,x) z)
 
 ;;;2.
-(defmacro ifcond )
+(defmacro ifcond (pred then else)
+  `(cond (,pred ,then)
+         (t ,else)))
+
+;;;3.
+(defmacro nth-expr (n &rest exprs)
+  `(eval (nth (1- ,n) (quote ,exprs))))
+
+;;;4.
+(defmacro ntimesdo (n &rest body)
+  (let ((g (gensym))
+        (h (gensym)))
+    `(let ((,h ,n))
+       (do ((,g 0 (+ ,g 1)))
+           ((>= ,g ,h))
+         ,@body))))
+
+(defmacro ntimesrec (n &rest body)
+  (princ n)
+  (let ((h (gensym)))
+    `(let ((,h ,n))
+       (princ ,h)
+       (if (= ,h 0)
+           nil
+           (progn
+             ,@body
+             (ntimesrec (1- ,h) ,@body)
+             )))))
+
+(defmacro ntimestest (n)
+  (princ n)
+  (let ((g (gensym)))
+    `(let ((,g ,n))
+      (princ ,g)
+      (if (= ,g 0)
+          nil
+          (ntimestest (1- ,g))))))
+#|
+(defun ntimesrec-helper (n &rest body)
+  (if (= n 0)
+      nil
+      (progn
+        body
+        (ntimesrec-helper (1- n) body))))
+|#
+(defmacro ntimesrec-helper (n &rest body)
+  `(if (= ,n 0)
+       nil
+       (progn
+         ,@body
+         (ntimesrec-helper (1- ,n) ,@body))))
